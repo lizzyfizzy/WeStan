a8d1d186113913ee31bd8a025b273c3c824c7097
Devorah Simon <devorahrsimon@gmail.com>
2023-05-17 21:24:24 -0700

    homepage images and icons and bug fixes

--- my-app/src/homepage.js
+++ my-app/src/homepage.js
@@ -1,5 +1,35 @@
 import React from 'react';
 
+import logo from './icons/crown logo.svg';
+import discover_logo from './icons/Union.svg';
+import profile_logo from './icons/Profile icon.svg';
+import yas_logo from './icons/yas icon.svg';
+import settings_logo from './icons/Gear Icon.svg';
+import main_logo from "./icons/Word Logo.svg";
+import audio_post from "./icons/audio_post.svg";
+import image_post from "./icons/image_post.svg";
+import text_post from "./icons/text_post.svg";
+import comments_light from "./icons/comments_light.svg";
+import yasses_light from "./icons/yasses_light.svg";
+import download_light from "./icons/download_light.svg";
+import download_dark from "./icons/download_dark.svg";
+import comments_dark from "./icons/comments_dark.svg";
+import yasses_dark from "./icons/yasses_dark.svg";
+
+import beyonce_tour from "./images/beyonce-tour.png"
+import gaga_image from "./images/gaga_image.svg"
+
+import anime_dude from "./images/profile_pics/anime_dude.svg"
+import blue_frog from "./images/profile_pics/blue_frog.svg"
+import daphne_frvr from "./images/profile_pics/daphne_frvr.svg"
+import devil_man from "./images/profile_pics/devil_man.svg"
+
+import lady_gaga from "./icons/Ellipse 15.svg";
+import ariana from "./icons/ariana.svg";
+import beyonce from "./icons/beyonce.svg";
+import cardi_b from "./icons/cardi_b.svg";
+import nicki_minaj from "./icons/Nicki_minaj.svg";
+
 function Homepage() {
     return (
         <div className="feed">
@@ -11,14 +41,19 @@ function Homepage() {
         <div className="post-container">
           <div className="remix-post post">
             <div className="left-bar">
-              <div className="artist-image">
+              <div className="artist-pic">
+                <img className="artist-content" src={lady_gaga} alt="lady_gaga"></img>
               </div>
-              <div className="type-indicator"></div>
+              <div className="type-indicator">
+                <img className="type-image" src={audio_post} alt="audio_post"></img>
+              </div>
             </div>
             <div className="main-content">
               <div className="remix-parent">
                 <div className="remix-content">
-                  <div className="remix-image"></div>
+                  <div className="remix-image"> {/*phase out*/}
+                    <img className="audio-image" src={gaga_image} alt="gaga_image"></img>
+                  </div>
                   <div className="remix-info">
                     <div className="remix-name text-content">
                       bad romance - breakcore edit
@@ -35,11 +70,19 @@ function Homepage() {
                 </div>
               </div>
               <div className="right-bar">
-                <div className="poster-profile right-box top"></div>
+                <div className="poster-profile right-box top">
+                  <img className="box-content" src={anime_dude} alt="anime_dude"></img>
+                </div>
                 <div className="right-filler"></div>
-                <div className="download right-box center"></div>
-                <div className="yasses right-box center"></div>
-                <div className="comments right-box bottom"></div>
+                <div className="download right-box center">
+                  <img className="box-content" src={download_dark} alt="download_dark"></img>
+                </div>
+                <div className="yasses right-box center">
+                  <img className="box-content" src={yasses_light} alt="yasses_light"></img>
+                </div>
+                <div className="comments right-box bottom">
+                  <img className="box-content" src={comments_light} alt="comments_light"></img>
+                </div>
               </div>
             </div>
             <div className="triangle-left">
@@ -67,7 +110,9 @@ function Homepage() {
             <div className="comments-arrow"></div>
             <div className="comments-container">
               <div className="write-comment">
-                <div className="right-box"></div>
+                <div className="my-profile right-box">
+                  <img className="box-content" src={daphne_frvr} alt="daphne_frvr"></img>
+                </div>
                 <input className="comment-input" type="text" value="Write a comment"></input>
               </div>
             </div>
@@ -76,20 +121,28 @@ function Homepage() {
         <div className="post-container">
           <div className="text-post post">
             <div className="left-bar">
-              <div className="artist-image">
-
+              <div className="artist-pic">
+                <img className="artist-content" src={ariana} alt="ariana"></img>
               </div>
-              <div className="type-indicator"></div>
+              <div className="type-indicator">
+                <img className="type-image" src={text_post} alt="text_post"></img>
+              </div>
             </div>
             <div className="main-content">
               <div className="text-content">
                 I just feel like Ariana Grande is god idk
               </div>
               <div className="right-bar">
-                <div className="poster-profile right-box top"></div>
+                <div className="poster-profile right-box top">
+                  <img className="box-content" src={devil_man} alt="devil_man"></img>
+                </div>
                 <div className="right-filler"></div>
-                <div className="yasses right-box center"></div>
-                <div className="comments right-box bottom"></div>
+                <div className="yasses right-box center">
+                  <img className="box-content" src={yasses_light} alt="yasses_light"></img>
+                </div>
+                <div className="comments right-box bottom">
+                  <img className="box-content" src={comments_dark} alt="comments_dark"></img>
+                </div>
               </div>
             </div>
           </div>
@@ -97,13 +150,17 @@ function Homepage() {
             <div className="comments-arrow"></div>
             <div className="comments-container">
               <div className="single-comment">
-                <div className="right-box"></div>
+                <div className="commenter-profile right-box">
+                  <img className="box-content" src={blue_frog} alt="blue_frog"></img>
+                </div>
                 <div className="comment-text text-content">
                   idk i think ur right!!!
                 </div>
               </div>
               <div className="write-comment">
-                <div className="right-box"></div>
+                <div className="my-profile right-box">
+                  <img className="box-content" src={daphne_frvr} alt="daphne_frvr"></img>
+                </div>
                 <input className="comment-input" type="text" value="Write a comment"></input>
               </div>
             </div>
@@ -112,24 +169,35 @@ function Homepage() {
         <div className="post-container">
           <div className="image-post post">
             <div className="left-bar">
-              <div className="artist-image">
+              <div className="artist-pic">
+                <img className="artist-content" src={beyonce} alt="beyonce"></img>
               </div>
-              <div className="type-indicator"></div>
+              <div className="type-indicator">
+                <img className="type-image" src={image_post} alt="image_post"></img>
+              </div>
             </div>
             <div className="main-content">
               <div className="image-parent">
-                <img alt="beyonce tour flyer" src="./beyonce-tour.png" className="image-content"></img>
+                <img alt="beyonce tour flyer" src={beyonce_tour} className="image-content"></img>
                 <div className="image-caption text-content">
                   WHO ELSE IS GOING OMGGGGG
                 </div>
               </div>
               
               <div className="right-bar">
-                <div className="poster-profile right-box top"></div>
+                <div className="poster-profile right-box top">
+                  <img className="box-content" src={daphne_frvr} alt="daphne_frvr"></img>
+                </div>
                 <div className="right-filler"></div>
-                <div className="download right-box center"></div>
-                <div className="yasses right-box center"></div>
-                <div className="comments right-box bottom"></div>
+                <div className="download right-box center">
+                  <img className="box-content" src={download_light} alt="download_light"></img>
+                </div>
+                <div className="yasses right-box center">
+                  <img className="box-content" src={yasses_dark} alt="yasses_dark"></img>
+                </div>
+                <div className="comments right-box bottom">
+                  <img className="box-content" src={comments_light} alt="comments_light"></img>
+                </div>
               </div>
             </div>
           </div>
@@ -137,7 +205,9 @@ function Homepage() {
             <div className="comments-arrow"></div>
             <div className="comments-container">
               <div className="write-comment">
-                <div className="right-box"></div>
+                <div className="my-profile right-box">
+                  <img className="box-content" src={daphne_frvr} alt="daphne_frvr"></img>
+                </div>
                 <input className="comment-input" type="text" value="Write a comment"></input>
               </div>
             </div>
